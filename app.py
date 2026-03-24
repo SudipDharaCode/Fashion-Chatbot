019a2232ff4db6e253d39991cf36b791e235bbc6
SudipDhara <127935611+SudipDharaCode@users.noreply.github.com>
2026-03-25 01:47:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List, Dict, Any
 
-from langgraph.graph import StateGraph
+from langgraph.graph import StateGraph, START, END
 from state import ChatState
 
 from nodes.memory import load_memory
@@ -13,10 +13,10 @@ from nodes.tool_node import tool_node
 from nodes.selector import selector_node
 from middleware.summarizer import save_memory
 
-
 # Build LangGraph
 
 builder = StateGraph(ChatState)
+
 
 builder.add_node("load_memory_node", load_memory)
 builder.add_node("intent_node", detect_intent)
@@ -26,7 +26,10 @@ builder.add_node("tool_node", tool_node)
 builder.add_node("selector_node", selector_node)
 builder.add_node("save_memory_node", save_memory)
 
+
 builder.set_entry_point("load_memory_node")
+
+builder.add_edge(START, "load_memory_node")
 
 builder.add_edge("load_memory_node", "intent_node")
 
@@ -39,6 +42,8 @@ builder.add_edge("llm_node", "selector_node")
 builder.add_edge("tool_node", "selector_node")
 
 builder.add_edge("selector_node", "save_memory_node")
+
+builder.add_edge("save_memory_node", END)
 
 graph = builder.compile()
 
@@ -89,6 +94,6 @@ def chat(req: ChatRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
 
 
