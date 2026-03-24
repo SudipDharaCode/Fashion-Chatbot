5cc216e1512ac2ec8bdc6abfd1849ca3743cb278
SudipDhara <127935611+SudipDharaCode@users.noreply.github.com>
2026-03-25 01:13:44 +0530

    Update llm_node.py

--- nodes/llm_node.py
+++ nodes/llm_node.py
@@ -2,7 +2,7 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
-    google_api_key="AIzaSyChqARrd2KT3_AUhJBhK_Kfg2PjdTL0K1o",
+    google_api_key="api_key",
     temperature=0.4
 )
 
